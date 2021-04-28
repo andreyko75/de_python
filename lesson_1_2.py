de983b5e69ad5e79b8668180ef4c6412fef0803e
Andrey <andreyko75@gmail.com>
2021-04-28 23:31:43 +0300

    correct_task_2

--- lesson_1_2.py
+++ lesson_1_2.py
@@ -4,7 +4,7 @@ while num_count < 1001:
     if num_count % 2 != 0:  # Заполняем список нечетными числами
         odd_list.append(num_count**3)
     num_count += 1
-print(odd_list)
+# print(odd_list) #для проверки что идут кубы
 sum_cube = 0
 sum_cube_plus_17 = 0
 # перебираем вкесь список
