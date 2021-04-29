d5a0d091862b3f547e031748258a0eca0f74d270
Andrey Koshkin <81832835+andreyko75@users.noreply.github.com>
2021-04-29 09:44:51 +0300

    Update lesson_1_2.py

--- lesson_1_2.py
+++ lesson_1_2.py
@@ -13,13 +13,13 @@ for number_from_list in odd_list:
     # перебираем цифры в числе из списка
     for odd_from_number in str(number_from_list):
         odds_sum = odds_sum + int(odd_from_number)
-        if odds_sum % 7 == 0:
-            sum_cube = sum_cube + number_from_list
+    if odds_sum % 7 == 0:
+        sum_cube = sum_cube + number_from_list
     odds_sum = 0
     # перебираем цифры в числе из списка увеличенного на 17
     for odd_from_number in str(number_from_list + 17):
         odds_sum = odds_sum + int(odd_from_number)
-        if odds_sum % 7 == 0:
-            sum_cube_plus_17 = sum_cube_plus_17 + number_from_list + 17
+    if odds_sum % 7 == 0:
+        sum_cube_plus_17 = sum_cube_plus_17 + number_from_list + 17
 print(sum_cube)
 print(sum_cube_plus_17)
